893899ecb23d32661b26cd7d0c9e554a7eeeb896
JJM <qotprldi92@gmail.com>
2024-10-11 21:30:21 +0900

    first commit

---
+++ src/test/java/com/example/mung/MungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
