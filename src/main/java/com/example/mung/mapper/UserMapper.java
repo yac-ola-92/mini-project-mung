aedfc4b92696af2ded63cce696d5bb996d049393
Yac.oLa <yac.ola.92@gmail.com>
2024-10-14 20:20:21 +0900

    Update UserMapper.java
    VO객체 타입 오류로 mapper 객체 손상으로 코드 삭제

--- src/main/java/com/example/mung/mapper/UserMapper.java
+++ src/main/java/com/example/mung/mapper/UserMapper.java
@@ -12,24 +12,5 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    // User 전체 조회
-    @Select("select * from user")
-    public List<UserDTO> listAll();
-
-    // User 상세 조회
-    @Select("select * from user where ${id}")
-    public UserDTO getOne(int id);
-
-    // User 등록
-    @Insert("INSERT INTO user (user_name, user_email, password, user_phone, user_birth, user_gender, nickname, role, user_address, profile_image_url, pet_info, business_number, business_sns_url) " +
-            "VALUES (#{user_name}, #{user_email}, #{password}, #{user_phone}, #{user_birth}, #{user_gender}, #{nickname}, #{role}, #{user_address}, #{profile_image_url}, #{pet_info}, #{business_number}, #{business_sns_url})")
-    public void insert(UserVO vo);
-
-    // User 수정
-    @Update("update user set user_name =#{user_name},user_email=${user_email},password=${password},user_phone=#{user_phone},user_birth=#{user_birth},user_gender=#{user_gender}" +
-            "nickname=#{nickname},role=#{role},user_address=#{user_address},profile_image_url=#{profile_image_url},pet_info=#{pet_info},business_number=#{business_number},business_sns_url=#{business_sns_url}" +
-            "where user_id = ${user_id}")
-    public void update(int user_id);
-    // User 삭제
 
 }
